b10618ced032d9465d3f63ace4845b690ab34f81
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-20 16:59:34 +0100

    removed doubles

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,19 +1,3 @@
-num_01 = 5
-num_02 = 7
-word_01 = "Hello "
-word_02 = " to"
-word_03 = " you"
-Bool_00 = False
-Bool_01 = True
-flout_01 = 9.5
-flout_02 = 5.5
-num_word_01 = "7"
-num_word_02 = "3"
-
-sum_01 = num_01 + num_02
-sentence_greeting = word_01 + word_02 + word_03
-calculate_words = int(num_word_01) + int(num_word_02)
-
 num_01 = 68
 num_02 = 25
 word_01 = "Hello"
