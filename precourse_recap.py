98b7c3efe729fe15da6cb336f40ec36d1b71d110
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-20 16:57:25 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,39 @@
+num_01 = 5
+num_02 = 7
+word_01 = "Hello "
+word_02 = " to"
+word_03 = " you"
+Bool_00 = False
+Bool_01 = True
+flout_01 = 9.5
+flout_02 = 5.5
+num_word_01 = "7"
+num_word_02 = "3"
+
+sum_01 = num_01 + num_02
+sentence_greeting = word_01 + word_02 + word_03
+calculate_words = int(num_word_01) + int(num_word_02)
+
+num_01 = 68
+num_02 = 25
+word_01 = "Hello"
+word_02 = " to "
+word_03 = "you"
+Bool_00 = False
+Bool_01 = True
+flout_01 = 9.5
+flout_02 = 5.5
+num_word_01 = "7"
+num_word_02 = "3"
+
+sum_01 = num_01 + num_02
+sum_02 = num_01 - num_02
+sum_03 = num_01 * num_01
+sentence_greeting = word_01 + word_02 + word_03
+calculate_words = int(num_word_01) + int(num_word_02)
+
+print(sum_01)
+print(sum_02)
+print(sum_03)
+print(sentence_greeting)
+print(calculate_words)
